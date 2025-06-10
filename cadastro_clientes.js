6d06a18ef114585d39d21c9a580da95e318d7301
Mg12345-web <contatomgmultas@gmail.com>
2025-06-10 14:51:49 -0300

    Update cadastro_clientes.js

--- cadastro_clientes.js
+++ cadastro_clientes.js
@@ -10,8 +10,8 @@ const LOCK_PATH = path.join(os.tmpdir(), 'pipefy_robo_clientes.lock');
 
 router.get('/start-clientes', async (req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.write('<pre>⏳ Aguardando 1 minuto para iniciar o robô de CLIENTES...
-');
+  res.write('<pre>⏳ Aguardando 1 minuto para iniciar o robô de CLIENTES...\n');
+
 
   function log(msg) {
     res.write(`${msg}\n`);
