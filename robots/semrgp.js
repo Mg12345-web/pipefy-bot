efe03485710ce60160e9efd7e0890995706f3334
Mg12345-web <contatomgmultas@gmail.com>
2025-06-18 12:48:21 -0300

    Update semrgp.js

--- robots/semrgp.js
+++ robots/semrgp.js
@@ -104,7 +104,7 @@ await clienteCard.click({ force: true });
 log(`✅ Cliente ${cpf} selecionado`);
 
 // Clique no cabeçalho do formulário, onde está "Sem RGP"
-await page.getByText('*Clientes', { exact: true }).click();
+await page.locator('div', { hasText: '* Clientes' }).first().click();
 await page.waitForTimeout(10000);
 
     // CRLV
