ae2dadcb2b807b93b07b60bdbb96ab41e317302c
Mg12345-web <contatomgmultas@gmail.com>
2025-06-22 11:18:58 -0300

    Update processoAdministrativo.js

--- robots/processoAdministrativo.js
+++ robots/processoAdministrativo.js
@@ -117,4 +117,4 @@ async function runProcessoAdministrativo(req, res) {
   }
 }
 
-module.exports = { runProcessoAdministrativo };
+module.exports = { runProcessoAdministrativoRobot: runProcessoAdministrativo };
